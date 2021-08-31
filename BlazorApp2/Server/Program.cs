a0070a69a24af385a21c3a3beeca524a82d82111
JT <Hawxy@users.noreply.github.com>
2021-08-31 18:37:33 +0800

    Add reproduction

---
+++ BlazorApp2/Server/Program.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.ResponseCompression;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseBlazorFrameworkFiles("/ui");
+app.UseStaticFiles();
+app.UseStaticFiles("/ui");
+
+app.UseRouting();
+
+
+app.MapRazorPages();
+app.MapControllers();
+app.MapFallbackToFile("ui/{*path:nonfile}", "ui/index.html").AllowAnonymous();
+
+app.Run();
